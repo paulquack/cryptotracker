1d56b0bd1e5985be0185ace7eb7bf37a89bf9e73
Paul Baker <paul@paulbaker.id.au>
2018-01-02 21:49:28 +1100

    more formatting

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ include('inc/header.php');
 $username='quackau';
 $user = new CryptoUser('quackau');
 echo "<table class=\"table\">
-    <tr><th>Account</th><th class="text-right">Balance</th></tr>\n";
+    <tr><th>Account</th><th class=\"text-right\">Balance</th></tr>\n";
 foreach($user->getAccounts() as $account){
-    printf("    <tr><td>%s (%s)</td><td class="text-right">%s</td></tr>\n",
+    printf("    <tr><td>%s (%s)</td><td class=\"text-right\">%s</td></tr>\n",
            $account->getNickname(), $account->getSymbol(),
            preg_replace('/(\.?0+)$/', '<span class="text-muted">$1</spam>', number_format($account->getBalance(),8)));
 }
