8f5165af2f04125bf89b92ca3f31c07f329c97dd
Paul Baker <paul@paulbaker.id.au>
2018-01-02 15:45:37 +1100

    de

--- inc/classes.php
+++ inc/classes.php
@@ -134,17 +134,19 @@ class CryptoAccount {
 
     public function getDailyBalance($start, $end){
         $statement = $this->getStatement();
+        var_export($statement);
         $i = 0;
         $startDay = floor(strtotime($start)/86400);
         $endDay = floor(strtotime($end)/86400);
         $result=array();
         for ($curDay = $startDay; $curDay <= $endDay; $curDay++){
-
+            $date = date('Y-m-d',$curDay*86400);
+            $curTxn = date('Y-m-d',$statement[$i-1]['timestamp']);
+            $bal = (($i>0) ? round($statement[$i-1]['balance'],8) : 0);
             while ($i < count($statement) and floor(strtotime($statement[$i]['timestamp'])/86400) <= $curDay){
                 $i++;
             }
-            echo "Using transaction " . ($i-1) . " Date: " . floor(strtotime($statement[$i]['timestamp'])/86400) . " > $curDay\n";
-            $result[date('Y-m-d',$curDay*86400)] = (($i>0) ? round($statement[$i-1]['balance'],8) : 0);
+            $result[$date] = $bal;
         }
         return $result;
     }
