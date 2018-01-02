e3363b7bc7b16e61fc0603903644bcf02eb7b2d3
Paul Baker <paul@paulbaker.id.au>
2018-01-02 20:31:40 +1100

    number format

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ $user = new CryptoUser('quackau');
 echo "<table class=\"table\">
     <tr><th>Account</th><th>Balance</th></tr>\n";
 foreach($user->getAccounts() as $account){
-    printf("    <tr><td>%s (%s)</td><td>%s</td></tr>\n",$account->getNickname(),$account->getSymbol(),number_format($account->getBalance(),8));
+    printf("    <tr><td>%s (%s)</td><td>%s</td></tr>\n",
+           $account->getNickname(), $account->getSymbol(),
+           trim(number_format($account->getBalance(),8),'0'));
 }
 echo "</table>\n";
 include('inc/footer.php')
