c10f59abc89537be6f8ff400a60e8f7b303a561d
Paul Baker <paul@paulbaker.id.au>
2018-01-03 21:08:31 +1100

    fix index

--- index.php
+++ index.php
@@ -7,9 +7,11 @@ $user = new CryptoUser('quackau');
 echo "<table class=\"table\">
     <tr><th>Account</th><th class=\"text-right\">Balance</th></tr>\n";
 foreach($user->getAccounts() as $account){
-    printf("    <tr><td>%s (%s)</td><td class=\"text-right\">%s</td></tr>\n",
-           $account->getNickname(), $account->getSymbol(),
-           preg_replace('/(\.?0+)$/', '<span class="text-muted">$1</spam>', number_format($account->getBalance(),8)));
+    if (count($account->getStatement()) > 0) {
+        printf("    <tr><td><a href=\"accountStatement.php?account=%u\">%s (%s)</a></td><td class=\"text-right\">%s</td></tr>\n",
+                $account->getId(), $account->getNickname(), $account->getSymbol(),
+                preg_replace('/(\.?0+)$/', '<span class="text-muted">$1</spam>', number_format($account->getBalance(),8)));
+    }
 }
 echo "</table>\n";
 include('inc/footer.php')
