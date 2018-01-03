3457d2ef062e2551c6ea2fc05810881c35e9cafd
Paul Baker <paul@paulbaker.id.au>
2018-01-03 22:03:04 +1100

    total row

--- index.php
+++ index.php
@@ -16,21 +16,25 @@ foreach($user->getAccounts() as $account){
 $symbols = implode(",",array_unique($symbols));
 $localcur = "AUD";
 $prices = (array)json_decode(file_get_contents("https://min-api.cryptocompare.com/data/pricemulti?fsyms=$symbols&tsyms=USD,$localcur"));
-var_export($prices);
+
+$balance=array('USD'=>0,$localcur=>0);
 
 echo "<table class=\"table\">
     <tr><th>Account</th><th class=\"text-right\">Balance</th><th class=\"text-right\">Price</th><th class=\"text-right\">Value</th></tr>\n";
 foreach($user->getAccounts() as $account){
     if (count($account->getStatement()) > 0) {
-        printf("    <tr><td><a href=\"accountStatement.php?account=%u\">%s (%s)</a></td><td class=\"text-right\">%s</td><td class=\"text-right\">%s<br><small>%s</small></td><td class=\"text-right\">%s<br><small>%s</small></td></tr>\n",
+        printf("    <tr>\n        <td><a href=\"accountStatement.php?account=%u\">%s (%s)</a></td><td class=\"text-right\">%s</td>\n" . _
+               "        <td class=\"text-right\">%s<br><small>%s</small></td><td class=\"text-right\">%s<br><small>%s</small></td>\n    </tr>\n",
                 $account->getId(), $account->getNickname(), $account->getSymbol(),
                 formatcurrency($account->getBalance()),
                 formatcurrency($prices[$account->getSymbol()]->USD),
                 formatcurrency($prices[$account->getSymbol()]->$localcur),
-                formatcurrency($account->getBalance() * $prices[$account->getSymbol()]->USD),
-                formatcurrency($account->getBalance() * $prices[$account->getSymbol()]->$localcur));
+                $balance['USD']+=formatcurrency($account->getBalance() * $prices[$account->getSymbol()]->USD),
+                $balance[$localcur]+=formatcurrency($account->getBalance() * $prices[$account->getSymbol()]->$localcur));
     }
 }
+printf("    <tr><th colspan=\"3\">Total</th><th class=\"text-right\">%s<br><small>%s</small></th></tr>\n",
+       formatcurrency($balance['USD']), formatcurrency($balance[$localcur]));
 echo "</table>\n";
 include('inc/footer.php')
 ?>
